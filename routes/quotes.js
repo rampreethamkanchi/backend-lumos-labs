91d367bef1e1c6d68dcf3eb4bf1e4b0a91674012
rampreetham <rampreethamkanchi@gmail.com>
2023-07-24 19:48:17 +0530

    This is first commit for backend of Lumos lab assignment

---
+++ routes/quotes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const QuoteInfo = require("../models/quotes");
+router.get("/",async (req,res)=>{
+    console.log("hello world");
+    // console.log('QuoteInfo.find()[0]:',await QuoteInfo.find()[0]);
+    res.status(200).send(await QuoteInfo.find());
+})
+router.post("/",async (req,res)=>{
+    const doc = new QuoteInfo({
+        author : req.body.author,
+        quote : req.body.quote
+    })
+    newQuote=await doc.save();
+    res.status(200).json(newQuote);
+    console.log('newQuote:', newQuote);
+})
+module.exports = router;
